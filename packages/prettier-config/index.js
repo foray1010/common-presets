31a1a21778ba1d015ae613fa3a23e55c51f7a603
Alex Young <foray1010@users.noreply.github.com>
2019-10-28 17:39:40 +0800

    fix(prettier-config): explicitly use some options

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,13 +1,18 @@
 'use strict'
 
 module.exports = {
+  arrowParens: 'avoid',
   bracketSpacing: true,
   endOfLine: 'lf',
+  jsxBracketSameLine: false,
   jsxSingleQuote: true,
   printWidth: 80,
+  quoteProps: 'as-needed',
   semi: false,
   singleQuote: true,
+  tabWidth: 2,
   trailingComma: 'all',
+  useTabs: false,
 
   overrides: [
     {
