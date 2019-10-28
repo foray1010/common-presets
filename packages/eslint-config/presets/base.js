deaffbd3aaf4069b740108bf9626990ac8e581bc
Alex Young <foray1010@users.noreply.github.com>
2019-10-28 22:31:24 +0800

    refactor: extract typescript config

--- packages/eslint-config/presets/base.js
+++ packages/eslint-config/presets/base.js
@@ -6,6 +6,43 @@ const { packageJson } = readPkgUp.sync()
 
 const isEsm = packageJson.type === 'module'
 
+const typescriptConfig = {
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/typescript',
+    'eslint-config-prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json', './tsconfig.*.json'],
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  rules: {
+    // sometime it takes too much manpower to get the type right and may prefer to fix it later
+    // also sometime the types from third-party libraries are wrong
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    // do not force third-party object (e.g. api response) to follow camel case
+    '@typescript-eslint/camelcase': ['error', { properties: 'never' }],
+    // prefer auto detect
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    // do not block functions referring to other functions
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        classes: true,
+        variables: true,
+        typedefs: true,
+      },
+    ],
+    // redundant for composing functions
+    '@typescript-eslint/unbound-method': 'off',
+  },
+}
+
 module.exports = {
   extends: [
     'eslint:recommended',
@@ -56,40 +93,7 @@ module.exports = {
     },
     {
       files: ['*.ts', '*.tsx'],
-      extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:import/typescript',
-        'eslint-config-prettier/@typescript-eslint',
-      ],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: ['./tsconfig.json', './tsconfig.*.json'],
-        sourceType: 'module',
-      },
-      plugins: ['@typescript-eslint/eslint-plugin'],
-      rules: {
-        // sometime it takes too much manpower to get the type right and may prefer to fix it later
-        // also sometime the types from third-party libraries are wrong
-        '@typescript-eslint/ban-ts-ignore': 'off',
-        // do not force third-party object (e.g. api response) to follow camel case
-        '@typescript-eslint/camelcase': ['error', { properties: 'never' }],
-        // prefer auto detect
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        // do not block functions referring to other functions
-        '@typescript-eslint/no-use-before-define': [
-          'error',
-          {
-            functions: false,
-            classes: true,
-            variables: true,
-            typedefs: true,
-          },
-        ],
-        // redundant for composing functions
-        '@typescript-eslint/unbound-method': 'off',
-      },
+      ...typescriptConfig,
     },
   ],
 }
