e54b16cba93e4dcb3c2309057fd2fd3ccb5571d9
Alex Young <foray1010@users.noreply.github.com>
2020-03-31 21:44:14 +0800

    feat: reenable rules that are incompatible with CHANGELOG.md

--- packages/remark-preset/index.js
+++ packages/remark-preset/index.js
@@ -6,10 +6,6 @@ module.exports = {
     'remark-preset-lint-markdown-style-guide',
     'remark-preset-lint-recommended',
     'remark-preset-prettier',
-    // compatible with common changelog format
-    ['remark-lint-heading-increment', false],
-    ['remark-lint-no-duplicate-headings', false],
-    ['remark-lint-no-multiple-toplevel-headings', false],
     // allow `_` on file name
     ['remark-lint-no-file-name-irregular-characters', false],
   ],
