31a1be3503d0290ee3f36aac2b1c46c35cbedf8c
Alex Young <foray1010@users.noreply.github.com>
2020-07-13 12:16:07 +0800

    style: do not use array if only one command in lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = {
-  '*.{json,yaml,yml}': ['yarn prettier --write'],
+  '*.{json,yaml,yml}': 'yarn prettier --write',
   '*.js': ['yarn prettier --write', 'yarn eslint --fix'],
   '*.md': (filenames) => {
     return [`yarn prettier --write ${filenames.join(' ')}`, 'yarn remark .']
