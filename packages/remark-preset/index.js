8582aacdb375eccec467d795b8eb3bf324805077
Alex Young <foray1010@users.noreply.github.com>
2020-10-28 11:25:14 +0800

    feat(remark-preset): support yaml frontmatter

--- packages/remark-preset/index.js
+++ packages/remark-preset/index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   plugins: [
+    'remark-frontmatter',
     'remark-preset-lint-consistent',
     'remark-preset-lint-markdown-style-guide',
     'remark-preset-lint-recommended',
