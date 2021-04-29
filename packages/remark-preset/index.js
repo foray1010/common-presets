fe2ec5fb28c33b812643cdf057b89397eef892ef
Alex Young <foray1010@users.noreply.github.com>
2021-04-29 10:45:03 +0800

    fix: cannot load remark preset

--- packages/remark-preset/index.js
+++ packages/remark-preset/index.js
@@ -6,7 +6,8 @@ module.exports = {
     'remark-preset-lint-consistent',
     'remark-preset-lint-markdown-style-guide',
     'remark-preset-lint-recommended',
-    'remark-preset-prettier',
+    // when using 'remark-preset-prettier' will throw `Error: Expected preset or plugin, not [object Object]`
+    require('remark-preset-prettier'),
     ['remark-lint-no-dead-urls', { skipLocalhost: true, skipOffline: true }],
     // allow `_` on file name
     ['remark-lint-no-file-name-irregular-characters', false],
