2282edfee21bd15c004ff56e02c7da012d701a56
Alex Young <foray1010@users.noreply.github.com>
2022-03-20 18:13:47 +0000

    Revert "chore: move to yarn v3" (#429)
    This reverts commit 9356fbfbe494f9c0110faad7e1cfe72f9790162c.

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,14 +2,11 @@
 
 module.exports = {
   '*.{cjs,cts,js,mjs,mts,ts,tsx}': [
-    'corepack yarn prettier --write',
-    'corepack yarn eslint --fix',
+    'yarn prettier --write',
+    'yarn eslint --fix',
   ],
-  '*.{json,yaml,yml}': 'corepack yarn prettier --write',
+  '*.{json,yaml,yml}': 'yarn prettier --write',
   '*.{markdown,md}'(filenames) {
-    return [
-      `corepack yarn prettier --write ${filenames.join(' ')}`,
-      'corepack yarn remark .',
-    ]
+    return [`yarn prettier --write ${filenames.join(' ')}`, 'yarn remark .']
   },
 }
