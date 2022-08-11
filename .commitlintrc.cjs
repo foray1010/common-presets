9213c4b4b53b58a988cb9484c5c6e8ca2ee2f83c
Alex Young <foray1010@users.noreply.github.com>
2022-08-11 20:36:47 +0100

    chore: populate commitlint scopes by workspaces definition

---
+++ .commitlintrc.cjs
@@ -0,0 +1,19 @@
+'use strict'
+
+const fg = require('fast-glob')
+const path = require('path')
+
+const packageJson = require('./package.json')
+
+const dirPaths = fg.sync(packageJson.workspaces, {
+  onlyFiles: false,
+  markDirectories: true,
+})
+const scopes = dirPaths.map((dirPath) => path.basename(dirPath))
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'always', scopes],
+  },
+}
