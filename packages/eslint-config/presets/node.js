00e14ee9c2f0afab8ebe78ecc89c3ab79b8a93f0
Alex Young <foray1010@users.noreply.github.com>
2022-10-01 12:00:47 +0100

    fix(eslint-config): better error description when using global Buffer or process

--- packages/eslint-config/presets/node.js
+++ packages/eslint-config/presets/node.js
@@ -3,6 +3,12 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   plugins: ['eslint-plugin-n'],
+  globals: {
+    // hack to mute no-undef error, and show n/prefer-global/buffer error instead
+    Buffer: 'readonly',
+    // hack to mute no-undef error, and show n/prefer-global/process error instead
+    process: 'readonly',
+  },
   rules: {
     // disallow deprecated node APIs
     'n/no-deprecated-api': 'error',
@@ -12,6 +18,10 @@ module.exports = {
     'n/no-unpublished-bin': 'error',
     // disallow unsupported Node.js built-in APIs on the specified version
     'n/no-unsupported-features/node-builtins': 'error',
+    // prefer `import { Buffer } from 'node:buffer'`
+    'n/prefer-global/buffer': ['error', 'never'],
+    // prefer `import process from 'node:process'`
+    'n/prefer-global/process': ['error', 'never'],
     // make `process.exit()` expressions the same code path as `throw`
     'n/process-exit-as-throw': 'error',
     // enforce shebang on the entry bin file
