67f003325cde0500284c06fe15fef0c9838c6bc4
Alex Young <foray1010@users.noreply.github.com>
2023-02-25 18:24:50 +0000

    fix(stylelint-config): use correct stylelint-config-concentric-order path

--- packages/stylelint-config/index.cjs
+++ packages/stylelint-config/index.cjs
@@ -5,7 +5,7 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-css-modules',
-    'stylelint-config-idiomatic-order',
+    'stylelint-config-concentric-order',
     'stylelint-prettier/recommended.js',
   ],
   plugins: ['stylelint-no-unsupported-browser-features'],
