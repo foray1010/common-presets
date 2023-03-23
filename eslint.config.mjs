95b9d967799a73b1c09aa90639b018ad2a623580
Alex Young <foray1010@users.noreply.github.com>
2023-03-23 16:55:24 +0000

    chore: cleanup unused eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import {
-  applyConfig,
   eslintFilesConfig,
   eslintIgnoresConfig,
   eslintNodeConfig,
-  eslintReactConfig,
 } from '@foray1010/eslint-config'
 
 const config = [
   ...eslintIgnoresConfig,
   ...eslintFilesConfig,
-  ...applyConfig(
-    {
-      ignorePrefixes: ['**/react'],
-    },
-    eslintNodeConfig,
-  ),
-  ...applyConfig(
-    {
-      filePrefixes: ['**/react'],
-    },
-    eslintReactConfig,
-  ),
+  ...eslintNodeConfig,
 ]
 export default config
