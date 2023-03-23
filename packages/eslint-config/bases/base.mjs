d3575954b9dac1290c77ff23ece9ce3e7c8fcc2f
Alex Young <foray1010@users.noreply.github.com>
2023-03-23 22:06:42 +0000

    fix(eslint-config): errors when using in project without typescript

--- packages/eslint-config/bases/base.mjs
+++ packages/eslint-config/bases/base.mjs
@@ -2,8 +2,6 @@ import js from '@eslint/js'
 import eslintPluginEslintComments from '@eslint-community/eslint-plugin-eslint-comments'
 // @ts-expect-error
 import { hasDep, isESM } from '@foray1010/common-presets-utils'
-import eslintPluginTypescriptEslint from '@typescript-eslint/eslint-plugin'
-import typescriptEslintParser from '@typescript-eslint/parser'
 import eslintPluginDeprecation from 'eslint-plugin-deprecation'
 import eslintPluginFunctional from 'eslint-plugin-functional'
 import eslintPluginImport from 'eslint-plugin-import'
@@ -17,6 +15,197 @@ import {
   typeScriptFileGlobs,
   typeScriptTestFileGlobs,
 } from '../constants.mjs'
+
+/** @returns {Promise<readonly import('eslint').Linter.FlatConfig[]>} */
+async function generateTypeScriptConfig() {
+  // typescript plugins are depended on `typescript` package
+  if (!hasDep('typescript')) return []
+
+  const eslintPluginTypescriptEslint = (
+    await import('@typescript-eslint/eslint-plugin')
+  ).default
+  const typescriptEslintParser = (await import('@typescript-eslint/parser'))
+    .default
+
+  return [
+    {
+      files: typeScriptFileGlobs,
+      languageOptions: {
+        /** @type {any} */
+        parser: typescriptEslintParser,
+        parserOptions: {
+          // faster linting on cli
+          // https://github.com/typescript-eslint/typescript-eslint/issues/3528
+          // turned off because @typescript-eslint/no-unsafe-* rules will output wrong errors
+          // allowAutomaticSingleRunInference: true,
+          project: ['./tsconfig*.json', './packages/*/tsconfig*.json'],
+        },
+      },
+      settings: {
+        'import/resolver': {
+          typescript: true,
+        },
+      },
+      plugins: {
+        // @ts-expect-error
+        '@typescript-eslint': eslintPluginTypescriptEslint,
+        // @ts-expect-error
+        deprecation: eslintPluginDeprecation,
+        functional: eslintPluginFunctional,
+      },
+      rules: {
+        ...eslintPluginTypescriptEslint.configs['eslint-recommended']?.rules,
+        ...eslintPluginTypescriptEslint.configs['recommended']?.rules,
+        ...eslintPluginTypescriptEslint.configs[
+          'recommended-requiring-type-checking'
+        ]?.rules,
+        ...eslintPluginImport.configs['typescript']?.rules,
+        // extend existing rule
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            types: {
+              // TypeScript team suggests to use `<T extends {}>` https://devblogs.microsoft.com/typescript/announcing-typescript-4-8/#unconstrained-generics-no-longer-assignable-to
+              '{}': false,
+            },
+            extendDefaults: true,
+          },
+        ],
+        // separate type exports which allow certain optimizations within compilers
+        '@typescript-eslint/consistent-type-exports': 'error',
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          {
+            // separate type imports which allow certain optimizations within compilers
+            prefer: 'type-imports',
+          },
+        ],
+        // disable the base rule as it can report incorrect errors, use @typescript-eslint/dot-notation instead
+        'dot-notation': 'off',
+        // only allow indexed syntax (e.g. `obj['key']`) for accessing undefined fields
+        '@typescript-eslint/dot-notation': [
+          'error',
+          {
+            allowIndexSignaturePropertyAccess: true,
+          },
+        ],
+        // encourage to use private accessibility modifier
+        '@typescript-eslint/explicit-member-accessibility': [
+          'error',
+          {
+            accessibility: 'explicit',
+          },
+        ],
+        // sometimes auto detect can provide a better and narrower type
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        // disallow duplicated value in enum as it is error-prone
+        '@typescript-eslint/no-duplicate-enum-values': 'error',
+        // need empty function for react context default value
+        '@typescript-eslint/no-empty-function': 'off',
+        // encourage to check error type before use in catch clauses
+        '@typescript-eslint/no-implicit-any-catch': 'error',
+        // when using typescript 5.0 with verbatimModuleSyntax flag on, compiler will not remove import statements with only inline type imports which lead to side effects
+        '@typescript-eslint/no-import-type-side-effects': 'error',
+        // enforce correct usage of `void` type
+        '@typescript-eslint/no-invalid-void-type': 'error',
+        // allow using async function as event handler in frontend
+        '@typescript-eslint/no-misused-promises': [
+          'error',
+          { checksVoidReturn: false },
+        ],
+        // declaration merging between classes and interfaces is unsafe
+        '@typescript-eslint/no-unsafe-declaration-merging': 'error',
+        // do not block functions referring to other functions
+        '@typescript-eslint/no-use-before-define': [
+          'error',
+          {
+            /* options from eslint/no-use-before-define */
+            // allow use function before defined as they could be hoisted
+            functions: false,
+            classes: true,
+            variables: true,
+
+            /* options from @typescript-eslint/no-use-before-define */
+            enums: true,
+            // confusing option, it will disable `typedefs`
+            ignoreTypeReferences: false,
+            // tsc allows types to be used before define
+            typedefs: false,
+          },
+        ],
+        // use with functional/prefer-readonly-type
+        // mark class variables as readonly if it is not mutated
+        '@typescript-eslint/prefer-readonly': 'error',
+        // make sure functions which return a promise will just return a rejected promise instead of throwing an error
+        '@typescript-eslint/promise-function-async': 'error',
+        // allow primitive value in template string
+        '@typescript-eslint/restrict-template-expressions': [
+          'error',
+          {
+            allowNumber: true,
+            allowBoolean: true,
+            allowAny: true, // mistakenly recognize string as any in 4.29.3
+            allowNullish: true,
+            allowRegExp: true,
+          },
+        ],
+        // avoid missed switch-case by requiring switch-case statements to be exhaustive with union type
+        '@typescript-eslint/switch-exhaustiveness-check': 'error',
+        // ignore static function as those are not supposed to use `this`
+        '@typescript-eslint/unbound-method': ['error', { ignoreStatic: true }],
+        // do not allow usage of deprecated code
+        'deprecation/deprecation': 'error',
+        // use with @typescript-eslint/prefer-readonly
+        'functional/prefer-readonly-type': [
+          'error',
+          {
+            // sometimes it is easier to mutate, it should be fine to mutate within local scope
+            allowLocalMutation: true,
+            // don't force library consumer to use readonly type
+            allowMutableReturnType: true,
+            // allow mutating class variables
+            ignoreClass: 'fieldsOnly',
+          },
+        ],
+        // forbid unnecessary callback wrapper
+        'functional/prefer-tacit': 'error',
+        'no-restricted-syntax': [
+          'error',
+          {
+            // encourage to use JS standard #private over TS private accessibility modifier, but excluding constructor because it cannot be private in JS: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields#description
+            selector:
+              ':matches(PropertyDefinition, MethodDefinition, TSParameterProperty)[accessibility="private"]:not([kind="constructor"])',
+            message:
+              'Use #private instead (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields)',
+          },
+        ],
+        // @typescript-eslint/eslint-plugin suggests to disable it: https://github.com/typescript-eslint/typescript-eslint/blob/2588e9ea55f78352fdd6ae92a306135aabb49a1a/docs/linting/TROUBLESHOOTING.md#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+        // It is disabled in recommended config but re-enabled here to enforce a subset of global variables that supported by both node.js and browsers
+        'no-undef': 'error',
+      },
+    },
+    {
+      files: typeScriptTestFileGlobs,
+      plugins: {
+        // @ts-expect-error
+        '@typescript-eslint': eslintPluginTypescriptEslint,
+        jest: eslintPluginJest,
+      },
+      rules: {
+        // doesn't work with jest.fn<void>()
+        '@typescript-eslint/no-invalid-void-type': 'off',
+        // replace by jest/unbound-method
+        '@typescript-eslint/unbound-method': 'off',
+        // allow passing an unbound method to `expect` calls
+        'jest/unbound-method': ['error', { ignoreStatic: true }],
+      },
+    },
+    {
+      files: typeScriptFileGlobs,
+      ...esmConfig,
+    },
+  ]
+}
 
 /** @type {import('eslint').Linter.FlatConfig} */
 const cjsConfig = {
@@ -51,7 +240,7 @@ const baseConfig = [
   js.configs.recommended,
   {
     languageOptions: {
-      ecmaVersion: 2021,
+      ecmaVersion: 2022,
       globals: {
         // should align with languageOptions.ecmaVersion
         ...globals.es2021,
@@ -199,188 +388,6 @@ const baseConfig = [
       'jest/prefer-hooks-on-top': 'error',
     },
   },
-  // typescript plugins are depended on `typescript` package
-  // @ts-expect-error
-  ...(hasDep('typescript')
-    ? [
-        {
-          files: typeScriptFileGlobs,
-          languageOptions: {
-            /** @type {any} */
-            parser: typescriptEslintParser,
-            parserOptions: {
-              // faster linting on cli
-              // https://github.com/typescript-eslint/typescript-eslint/issues/3528
-              // turned off because @typescript-eslint/no-unsafe-* rules will output wrong errors
-              // allowAutomaticSingleRunInference: true,
-              project: ['./tsconfig*.json', './packages/*/tsconfig*.json'],
-            },
-          },
-          settings: {
-            'import/resolver': {
-              typescript: true,
-            },
-          },
-          plugins: {
-            '@typescript-eslint': eslintPluginTypescriptEslint,
-            deprecation: eslintPluginDeprecation,
-            functional: eslintPluginFunctional,
-          },
-          rules: {
-            ...eslintPluginTypescriptEslint.configs['eslint-recommended']
-              ?.rules,
-            ...eslintPluginTypescriptEslint.configs['recommended']?.rules,
-            ...eslintPluginTypescriptEslint.configs[
-              'recommended-requiring-type-checking'
-            ]?.rules,
-            ...eslintPluginImport.configs['typescript']?.rules,
-            // extend existing rule
-            '@typescript-eslint/ban-types': [
-              'error',
-              {
-                types: {
-                  // TypeScript team suggests to use `<T extends {}>` https://devblogs.microsoft.com/typescript/announcing-typescript-4-8/#unconstrained-generics-no-longer-assignable-to
-                  '{}': false,
-                },
-                extendDefaults: true,
-              },
-            ],
-            // separate type exports which allow certain optimizations within compilers
-            '@typescript-eslint/consistent-type-exports': 'error',
-            '@typescript-eslint/consistent-type-imports': [
-              'error',
-              {
-                // separate type imports which allow certain optimizations within compilers
-                prefer: 'type-imports',
-              },
-            ],
-            // disable the base rule as it can report incorrect errors, use @typescript-eslint/dot-notation instead
-            'dot-notation': 'off',
-            // only allow indexed syntax (e.g. `obj['key']`) for accessing undefined fields
-            '@typescript-eslint/dot-notation': [
-              'error',
-              {
-                allowIndexSignaturePropertyAccess: true,
-              },
-            ],
-            // encourage to use private accessibility modifier
-            '@typescript-eslint/explicit-member-accessibility': [
-              'error',
-              {
-                accessibility: 'explicit',
-              },
-            ],
-            // sometimes auto detect can provide a better and narrower type
-            '@typescript-eslint/explicit-module-boundary-types': 'off',
-            // disallow duplicated value in enum as it is error-prone
-            '@typescript-eslint/no-duplicate-enum-values': 'error',
-            // need empty function for react context default value
-            '@typescript-eslint/no-empty-function': 'off',
-            // encourage to check error type before use in catch clauses
-            '@typescript-eslint/no-implicit-any-catch': 'error',
-            // when using typescript 5.0 with verbatimModuleSyntax flag on, compiler will not remove import statements with only inline type imports which lead to side effects
-            '@typescript-eslint/no-import-type-side-effects': 'error',
-            // enforce correct usage of `void` type
-            '@typescript-eslint/no-invalid-void-type': 'error',
-            // allow using async function as event handler in frontend
-            '@typescript-eslint/no-misused-promises': [
-              'error',
-              { checksVoidReturn: false },
-            ],
-            // declaration merging between classes and interfaces is unsafe
-            '@typescript-eslint/no-unsafe-declaration-merging': 'error',
-            // do not block functions referring to other functions
-            '@typescript-eslint/no-use-before-define': [
-              'error',
-              {
-                /* options from eslint/no-use-before-define */
-                // allow use function before defined as they could be hoisted
-                functions: false,
-                classes: true,
-                variables: true,
-
-                /* options from @typescript-eslint/no-use-before-define */
-                enums: true,
-                // confusing option, it will disable `typedefs`
-                ignoreTypeReferences: false,
-                // tsc allows types to be used before define
-                typedefs: false,
-              },
-            ],
-            // use with functional/prefer-readonly-type
-            // mark class variables as readonly if it is not mutated
-            '@typescript-eslint/prefer-readonly': 'error',
-            // make sure functions which return a promise will just return a rejected promise instead of throwing an error
-            '@typescript-eslint/promise-function-async': 'error',
-            // allow primitive value in template string
-            '@typescript-eslint/restrict-template-expressions': [
-              'error',
-              {
-                allowNumber: true,
-                allowBoolean: true,
-                allowAny: true, // mistakenly recognize string as any in 4.29.3
-                allowNullish: true,
-                allowRegExp: true,
-              },
-            ],
-            // avoid missed switch-case by requiring switch-case statements to be exhaustive with union type
-            '@typescript-eslint/switch-exhaustiveness-check': 'error',
-            // ignore static function as those are not supposed to use `this`
-            '@typescript-eslint/unbound-method': [
-              'error',
-              { ignoreStatic: true },
-            ],
-            // do not allow usage of deprecated code
-            'deprecation/deprecation': 'error',
-            // use with @typescript-eslint/prefer-readonly
-            'functional/prefer-readonly-type': [
-              'error',
-              {
-                // sometimes it is easier to mutate, it should be fine to mutate within local scope
-                allowLocalMutation: true,
-                // don't force library consumer to use readonly type
-                allowMutableReturnType: true,
-                // allow mutating class variables
-                ignoreClass: 'fieldsOnly',
-              },
-            ],
-            // forbid unnecessary callback wrapper
-            'functional/prefer-tacit': 'error',
-            'no-restricted-syntax': [
-              'error',
-              {
-                // encourage to use JS standard #private over TS private accessibility modifier, but excluding constructor because it cannot be private in JS: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields#description
-                selector:
-                  ':matches(PropertyDefinition, MethodDefinition, TSParameterProperty)[accessibility="private"]:not([kind="constructor"])',
-                message:
-                  'Use #private instead (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields)',
-              },
-            ],
-            // @typescript-eslint/eslint-plugin suggests to disable it: https://github.com/typescript-eslint/typescript-eslint/blob/2588e9ea55f78352fdd6ae92a306135aabb49a1a/docs/linting/TROUBLESHOOTING.md#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-            // It is disabled in recommended config but re-enabled here to enforce a subset of global variables that supported by both node.js and browsers
-            'no-undef': 'error',
-          },
-        },
-        {
-          files: typeScriptTestFileGlobs,
-          plugins: {
-            '@typescript-eslint': eslintPluginTypescriptEslint,
-            jest: eslintPluginJest,
-          },
-          rules: {
-            // doesn't work with jest.fn<void>()
-            '@typescript-eslint/no-invalid-void-type': 'off',
-            // replace by jest/unbound-method
-            '@typescript-eslint/unbound-method': 'off',
-            // allow passing an unbound method to `expect` calls
-            'jest/unbound-method': ['error', { ignoreStatic: true }],
-          },
-        },
-        {
-          files: typeScriptFileGlobs,
-          ...esmConfig,
-        },
-      ]
-    : []),
+  ...(await generateTypeScriptConfig()),
 ]
 export default baseConfig
