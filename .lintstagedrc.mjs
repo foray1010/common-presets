29cd57bee888b14baf87e7ca17e90a4568e30508
Alex Young <foray1010@users.noreply.github.com>
2023-06-27 11:54:49 +0100

    test(stylelint-config): sanity check

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,6 @@
 const config = {
   '*.{cjs,cts,js,mjs,mts,ts,tsx}': ['yarn prettier --write', 'eslint --fix'],
+  '*.css': ['yarn prettier --write', 'yarn stylelint --fix'],
   '*.{json,yaml,yml}': 'yarn prettier --write',
   '*.{markdown,md}': ['yarn prettier --write', 'yarn remark'],
 }
