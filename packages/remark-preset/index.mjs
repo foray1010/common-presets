823207b4196f4f74e63df92601e36b788de2b123
Alex Young <foray1010@users.noreply.github.com>
2025-03-14 09:25:21 +0800

    fix(remark-preset): remove remark-lint-no-dead-urls because of false positives

--- packages/remark-preset/index.mjs
+++ packages/remark-preset/index.mjs
@@ -5,7 +5,6 @@ const preset = {
     'remark-preset-lint-markdown-style-guide',
     'remark-preset-lint-recommended',
     'remark-preset-prettier',
-    ['remark-lint-no-dead-urls', { skipLocalhost: true, skipOffline: true }],
     // allow `_` on file name
     ['remark-lint-no-file-name-irregular-characters', false],
     // allow title case on file name
