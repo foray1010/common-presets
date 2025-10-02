1d7a1df2febe9fddf10ad487ee835e329bad9f2c
Alex Young <foray1010@users.noreply.github.com>
2025-10-02 20:02:23 +0800

    feat(deps): update dependency eslint-plugin-react-hooks to v6

--- packages/eslint-config/types/eslint-plugins.d.ts
+++ packages/eslint-config/types/eslint-plugins.d.ts
@@ -1,36 +1,15 @@
-declare module '@eslint-community/eslint-plugin-*' {
+declare module 'eslint-plugin-react-hooks' {
   import type { Linter } from 'eslint'
 
-  export const configs: Record<
+  const configs: Record<
     string,
     {
       readonly rules: Linter.RulesRecord
     }
   >
-}
 
-declare module 'eslint-plugin-*' {
-  import type { Linter } from 'eslint'
-
-  export const configs: Record<
-    string,
-    {
-      readonly rules: Linter.RulesRecord
-    }
-  >
-}
-
-declare module 'eslint-plugin-react' {
-  import type { Linter } from 'eslint'
-
-  export const configs: {
-    readonly flat: {
-      readonly 'jsx-runtime': {
-        readonly rules: Linter.RulesRecord
-      }
-      readonly recommended: {
-        readonly rules: Linter.RulesRecord
-      }
-    }
+  const defaultExport = {
+    configs,
   }
+  export default defaultExport
 }
