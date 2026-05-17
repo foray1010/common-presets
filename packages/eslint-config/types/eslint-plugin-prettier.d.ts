cdbd895aeb12743b006116fa1a7b7c75d45d4062
Alex Young <foray1010@users.noreply.github.com>
2026-05-17 22:21:35 +0800

    chore(eslint-config): cleanup unnecessary type files

--- packages/eslint-config/types/eslint-plugin-prettier.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'eslint-plugin-prettier/recommended' {
-  import type { ConfigObject, RulesConfig } from 'eslint/config'
-
-  const config: ConfigObject<RulesConfig>
-  export default config
-}
--- packages/eslint-config/types/eslint-plugin-simple-import-sort.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'eslint-plugin-simple-import-sort' {
-  import type { Plugin } from 'eslint/config'
-
-  const plugin: Plugin
-  export default plugin
-}
